8948f2874dafbb913afdfddbb8e1a91da9e1873d
Mauro Insacco <mauro.insacco@gmail.com>
2026-03-02 07:59:01 +0100

    wip

--- v2/start.sh
+++ v2/start.sh
@@ -8,18 +8,23 @@ echo "Starting MetaTrader 5 in headless mode..."
 
 # set up display
 export DISPLAY=:100
-Xvfb :100 -ac -screen 0 1024x768x24 &
+# Xvfb :100 -ac -screen 0 1024x768x24 &
+Xvfb :100 -ac -screen 0 1x1x8 &
 
 
-unzip /mt5docker/meta.zip -d '/opt/wineprefix/drive_c/Program Files' && \
-mv "/opt/wineprefix/drive_c/Program Files/meta" "/opt/wineprefix/drive_c/Program Files/MetaTrader 5"
-rm -f "/opt/wineprefix/drive_c/Program Files/MetaTrader 5/Config/servers.dat" && \
-cp /volumes/servers.dat "/opt/wineprefix/drive_c/Program Files/MetaTrader 5/Config/servers.dat" && \
-rm -f "/opt/wineprefix/drive_c/Program Files/MetaTrader 5/Config/common.ini" && \
-cp /volumes/common.ini "/opt/wineprefix/drive_c/Program Files/MetaTrader 5/Config/common.ini"
+if [ ! -d "/opt/wineprefix/drive_c/Program Files/meta101" ]; then
+  unzip /mt5docker/meta.zip -d '/opt/wineprefix/drive_c/Program Files' && \
+  mv "/opt/wineprefix/drive_c/Program Files/meta" "/opt/wineprefix/drive_c/Program Files/MetaTrader 5"
+  rm -f "/opt/wineprefix/drive_c/Program Files/MetaTrader 5/Config/servers.dat" && \
+  cp /volumes/servers.dat "/opt/wineprefix/drive_c/Program Files/MetaTrader 5/Config/servers.dat" && \
+  rm -f "/opt/wineprefix/drive_c/Program Files/MetaTrader 5/Config/common.ini" && \
+  cp /volumes/common.ini "/opt/wineprefix/drive_c/Program Files/MetaTrader 5/Config/common.ini"
 
-mv "/opt/wineprefix/drive_c/Program Files/MetaTrader 5" "/opt/wineprefix/drive_c/Program Files/meta"
-cp -r "/opt/wineprefix/drive_c/Program Files/meta" "/opt/wineprefix/drive_c/Program Files/meta101"
+  mv "/opt/wineprefix/drive_c/Program Files/MetaTrader 5" "/opt/wineprefix/drive_c/Program Files/meta"
+  cp -r "/opt/wineprefix/drive_c/Program Files/meta" "/opt/wineprefix/drive_c/Program Files/meta101"
+fi
+
+sleep 1
 
 wine python /mt5docker/script.py
 
